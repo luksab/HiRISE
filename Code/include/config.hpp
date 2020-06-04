ceca1c85f059e69ac17c064b78cb92edbff971e9
luksab <luksablp@gmail.com>
2020-06-04 15:07:01 +0200

    add gitignore

--- Code/include/config.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-
-#define SHADER_ROOT std::string("/home/lukas/ownCloud/Semester 4/ComputerGraphik/HiRISE/Code/shaders/")
-#define DATA_ROOT std::string("/home/lukas/ownCloud/Semester 4/ComputerGraphik/HiRISE/Code/data/")
