61810febd769fece9e6de32b27389e239cbaa846
luksab <luksablp@gmail.com>
2020-07-06 23:02:48 +0200

    fix ffmpeg script framerate

--- Code/ffmpeg.sh
+++ Code/ffmpeg.sh
@@ -1,1 +1,1 @@
-ffmpeg -y -i data/screenshots/s%06d.png -c:v libx264 -preset veryslow -pix_fmt yuv420p -vf fps=60 -crf 20 data/screenshots/out.mp4
+ffmpeg -y -r 60 -i data/screenshots/s%06d.png -c:v libx264 -preset veryslow -r 60 -pix_fmt yuv420p -crf 27 data/screenshots/out.mp4
